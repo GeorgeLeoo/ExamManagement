aa8c525bb3aaed6623015eeb53f2baa2eae4da82
GeorgeLeeo <georgeleeo@163.com>
2020-02-28 11:22:17 +0800

    init2

---
+++ src/icons/components/home.ts
@@ -0,0 +1,12 @@
+/* eslint-disable */
+/* tslint:disable */
+// @ts-ignore
+import icon from 'vue-svgicon'
+icon.register({
+  'home': {
+    width: 16,
+    height: 14,
+    viewBox: '0 0 16 14',
+    data: '<path pid="0" d="M2.641 8.313v4.383h2.33v-2.617h5.977v2.617h2.33V8.274L7.963 4.496 2.641 8.313zm5.318-5.43l6.649 4.725V14h-4.99v-2.617H6.301V14H1.31V7.651l6.65-4.769-.001.001zM.776 6.672L0 5.612 7.957 0 16 5.612l-.77 1.062-7.268-5.07L.776 6.672z" _fill="#FFF" fill-rule="nonzero"/>'
+  }
+})
