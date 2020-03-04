dac2f35bfdfdecdda8a35016dbe4e931b3b8adfd
GeorgeLeeo <georgeleeo@163.com>
2020-03-04 14:10:38 +0800

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,1 +1,11 @@
-#!/usr/bin/expect -fset timeout 30scp -r "../${siteSource}/." server@139.159.201.22:/root/www/ExamManagementexpect "*password:"send "123loveyou,\r"expect eof
+#!/usr/bin/expect
+
+set timeout 30
+
+spawn scp "../${siteSource}/." root@139.159.201.22:/root/www/ExamManagement
+
+expect "password:"
+
+send "123loveyou,\r"
+
+interact
