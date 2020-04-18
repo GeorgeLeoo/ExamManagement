5610a6a7922e8870b3c3946c30ee6217a41ebf9a
GeorgeLeoo <georgeleeo@163.com>
2020-04-19 00:22:51 +0800

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,13 @@
 
 set timeout 2000
 
+spawn zip -r ExamManagement.zip ./ExamManagement
+
 spawn scp -r ./ExamManagement root@139.159.201.22:/root/www
+
+spawn unzip /root/www/ExamManagement
+
+spawn rm /root/www/ExamManagement.zip
 
  expect {
 
